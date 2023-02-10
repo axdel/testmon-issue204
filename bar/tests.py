3015de708c272a48b567d8e08518e6903752bdf4
Tibor Arpas <tibor.arpas@infinit.sk>
2023-02-10 09:01:48 +0100

    remove unecessary stuff

--- bar/tests.py
+++ bar/tests.py
@@ -1,22 +1,10 @@
 from django.test import TestCase as DjangoTestCase
-from foo.tests import *
+from foo import tests as foo_methods
 
 
 class BarTestCase(DjangoTestCase):
 
-    def setUp(self):
-        self.foo_tests = FooTestCase()
-
+    
     def test_bar_1(self):
-        self.foo_tests = FooTestCase()
-        self.foo_tests.test_foo_1()
-        assert True
-
-    def test_bar_2(self):
-        assert True
-
-    def test_bar_3(self):
-        assert True
-
-    def test_bar_4(self):
+        foo_methods.bait()
         assert True
--- foo/tests.py
+++ foo/tests.py
@@ -6,11 +6,6 @@ class FooTestCase(DjangoTestCase):
     def test_foo_1(self):
         assert True
 
-    def test_foo_2(self):
-        assert True
 
-    def test_foo_3(self):
-        assert True
-
-    def test_foo_4(self):
-        assert True
+def bait():
+    pass
